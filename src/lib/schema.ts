ec34ee97e06e10a1c15b9a77525064a3efd6c309
Martin Fink <martin.fink@posteo.de>
2023-10-13 11:27:04 +0200

    wip

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -3,46 +3,48 @@ import { z } from 'zod';
 const BlockTextSchema = z.object({
   id: z.number(),
   text: z.string(),
+  design: z.enum(['style_1', 'style_2', 'style_3', 'style_4', 'style_5']),
 });
 export type BlockText = z.infer<typeof BlockTextSchema>;
 
 const CTAInternalSchema = z.object({
   collection: z.enum(['element_cta_internal']),
-    item: z.object({
-        id: z.number(),
-        title: z.string(),
-        linked_page: z.object({
-            url: z.string(),
-        }).nullable(),
-    }),
+  item: z.object({
+    id: z.number(),
+    title: z.string(),
+    linked_page: z
+      .object({
+        url: z.string(),
+      })
+      .nullable(),
+  }),
 });
 export type CTAInternal = z.infer<typeof CTAInternalSchema>;
 
 const CTAExternalSchema = z.object({
-    collection: z.enum(['element_cta_external']),
-    item: z.object({
-        id: z.number(),
-        title: z.string(),
-        url: z.string(),
-    }),
+  collection: z.enum(['element_cta_external']),
+  item: z.object({
+    id: z.number(),
+    title: z.string(),
+    url: z.string(),
+  }),
 });
 export type CTAExternal = z.infer<typeof CTAExternalSchema>;
 
-const CTASchema = z.lazy(() =>
-    z.union([
-      CTAExternalSchema,
-      CTAInternalSchema,
-    ])
-);
+const CTASchema = z.lazy(() => z.union([CTAExternalSchema, CTAInternalSchema]));
 
 const BlockHeroSchema = z.object({
   id: z.number(),
-  image: z.object({
-    id: z.string(),
-    width: z.number(),
-    height: z.number(),
-  }).nullable(),
-  image_styles: z.array(z.enum(['rounded_bl', 'rounded_br', 'rounded_tl', 'rounded_tr'])).nullable(),
+  image: z
+    .object({
+      id: z.string(),
+      width: z.number(),
+      height: z.number(),
+    })
+    .nullable(),
+  image_styles: z
+    .array(z.enum(['rounded_bl', 'rounded_br', 'rounded_tl', 'rounded_tr']))
+    .nullable(),
   title: z.string().nullable(),
   subtitle: z.string().nullable(),
   ctas: z.array(CTASchema),
@@ -52,12 +54,7 @@ export type BlockHero = z.infer<typeof BlockHeroSchema>;
 const BlockSchema = z.object({
   id: z.number(),
   sections_id: z.number(),
-  item: z.lazy(() =>
-    z.union([
-      BlockTextSchema,
-      BlockHeroSchema,
-    ])
-  ),
+  item: z.lazy(() => z.union([BlockTextSchema, BlockHeroSchema])),
   collection: z.enum(['block_text', 'block_hero']),
   sort: z.number().nullable(),
 });
@@ -73,9 +70,11 @@ const TranslationSchema = z.object({
   show_title: z.boolean(),
   title: z.string(),
   url: z.string(),
-  sections: z.array(z.object({
+  sections: z.array(
+    z.object({
       sections_id: SectionSchema,
-    })),
+    }),
+  ),
 });
 
 const DataSchema = z.object({
