6b202b3d568f5cc94f7eb2ef1a1dd5406637cae7
Martin Fink <martin.fink@posteo.de>
2023-10-16 09:58:27 +0200

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,14 +7,22 @@ import { defineConfig } from 'astro/config';
 import robotsConfig from './robots-txt.config';
 
 // https://astro.build/config
+const isProduction = process.env.NODE_ENV === 'production';
+const isDraft = process.env.NODE_ENV === 'draft';
+const isDev = !isProduction && !isDraft;
+
+let adapter = node({ mode: 'standalone' });
+
+if (isProduction) {
+  adapter = vercelStatic();
+}
+
+if (isDraft) {
+  adapter = vercel();
+}
 
 export default defineConfig({
   integrations: [tailwind(), robotsTxt(robotsConfig)],
-  output: process.env.NODE_ENV === 'production' ? 'static' : 'server',
-  adapter:
-    process.env.NODE_ENV === 'production'
-      ? vercelStatic()
-      : process.env.NODE_ENV === 'draft'
-      ? vercel()
-      : node({ mode: 'standalone' })
+  output: isProduction ? 'static' : 'server',
+  adapter
 });
